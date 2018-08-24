b15d9229b92cb7b2fc8fad105abf056126689d26
Milan Vit <milanvit@milanvit.net>
2018-08-24 17:08:26 +0900

    Replaced `EventLoopFuture`s with `Future`s

--- Sources/App/Repositories/TodoRepository.swift
+++ Sources/App/Repositories/TodoRepository.swift
@@ -8,15 +8,15 @@ protocol TodoRepository: Service {
 }
 
 final class SQLiteTodoRepository: TodoRepository {
-    func getAll(on connectable: DatabaseConnectable) -> EventLoopFuture<[Todo]> {
+    func getAll(on connectable: DatabaseConnectable) -> Future<[Todo]> {
         return Todo.query(on: connectable).all()
     }
 
-    func create(_ todo: Todo, on connectable: DatabaseConnectable) -> EventLoopFuture<Todo> {
+    func create(_ todo: Todo, on connectable: DatabaseConnectable) -> Future<Todo> {
         return todo.save(on: connectable)
     }
 
-    func delete(_ todo: Todo, on connectable: DatabaseConnectable) -> EventLoopFuture<Void> {
+    func delete(_ todo: Todo, on connectable: DatabaseConnectable) -> Future<Void> {
         return todo.delete(on: connectable)
     }
 }
